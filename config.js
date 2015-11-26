3eacbf91259f8e5e5d70bcd9d46dad256b771a4d
Nikolaj Ivancic <nik@congral.com>
2015-11-25 23:03:37 -0500

    Update config.js
    Babel options were incomplete - missed the es7 stuff

--- config.js
+++ config.js
@@ -4,7 +4,9 @@ System.config({
   babelOptions: {
     "optional": [
       "runtime",
-      "optimisation.modules.system"
+      "optimisation.modules.system",
+      "es7.decorators",
+      "es7.classProperties"      
     ]
   },
   paths: {
