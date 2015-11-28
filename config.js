7d515c6584de9171f018606604c2e1cdb06eb579
Jeroen <Jeroenvinke17@gmail.com>
2015-11-28 19:20:51 +0100

    fix(dep): add jquery dependency

--- config.js
+++ config.js
@@ -26,6 +26,7 @@ System.config({
     "babel": "npm:babel-core@5.8.22",
     "babel-runtime": "npm:babel-runtime@5.8.20",
     "core-js": "npm:core-js@1.2.6",
+    "jquery": "github:components/jquery@2.1.4",
     "showdown": "github:showdownjs/showdown@1.3.0",
     "traceur": "github:jmcriffey/bower-traceur@0.0.91",
     "traceur-runtime": "github:jmcriffey/bower-traceur-runtime@0.0.91",
