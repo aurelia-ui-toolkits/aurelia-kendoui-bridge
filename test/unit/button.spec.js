e33e5745e626d6123911a24dc4a8d0f02267a50c
Jeroen <Jeroenvinke17@gmail.com>
2015-11-29 12:31:28 +0100

    feat(unit-test): add unit test

---
+++ test/unit/button.spec.js
@@ -0,0 +1,29 @@
+import {AuKendoButton as Widget} from 'src/button/button';
+import {initialize} from 'aurelia-pal-browser';
+import {DOM} from 'aurelia-pal';
+import {Container} from 'aurelia-dependency-injection';
+import {TemplatingEngine} from 'aurelia-templating';
+
+let element = document.createElement('div');
+
+describe('Button', () => {
+
+  let widget, container, templatingEngine;
+
+  beforeEach(() => {
+      initialize();
+      container = new Container();
+      container.registerInstance(DOM.Element, element);
+      templatingEngine = container.get(TemplatingEngine);
+
+      widget = templatingEngine.createViewModelForUnitTest(Widget);
+  });
+
+  it('clears options in constructor', () => {
+      expect(widget.options).toEqual({});
+  });
+
+  it('sets element in constructor', () => {
+      expect(widget.element).toBe(element);
+  });
+});
