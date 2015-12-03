3d61d9fef7f15bd9aa8bad39624b0532ce69155c
Nikolaj Ivancic <nik@congral.com>
2015-12-02 21:23:32 -0500

    Update tab-position.js

--- sample/src/tabstrip/tab-position.js
+++ sample/src/tabstrip/tab-position.js
@@ -1,11 +1,3 @@
 export class TabPosition {
 
-    // if (this.id==="tabstrip-left") {
-    //     console.log("LEFT");
-    // } else if (this.id==="tabstrip-right") {
-    //         console.log("RIGHT");
-    //     else if (this.id==="tabstrip-bottom") {
-    //         console.log("BOTTOM");
-    //     }
-    // }
 }
