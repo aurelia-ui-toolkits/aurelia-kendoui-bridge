9061923ab5841b1ed6f8f1824a72d05c6f78b3e9
Nik <nik@congral.com>
2015-12-05 22:57:39 -0500

    improved grid a little

--- sample/src/grid/basic-use.js
+++ sample/src/grid/basic-use.js
@@ -1,3 +1,13 @@
 export class BasicUse {
-	
+  datasource;
+
+  constructor() {
+    this.datasource = {
+      type: "odata",
+      serverFiltering: true,
+      transport: {
+        read: "//demos.telerik.com/kendo-ui/service/Northwind.svc/Customers"
+      }
+    };
+  }
 }
--- sample/src/grid/index.js
+++ sample/src/grid/index.js
@@ -3,14 +3,14 @@ import {useView} from 'aurelia-framework';
 @useView("shared/showcase.html")
 export class Index {
 
-	configureRouter(config, router){
-	    config.title = 'Grid';
-	    config.map([
-	      { route: ['','basic-use'],  moduleId: './basic-use',      nav: true, title:'Basic use' },
-	      { route: 'html-config',  moduleId: './html-config',      nav: true, title:'HTML configuration' },
-	      { route: 'templating-issue',  moduleId: './templating-issue',      nav: true, title:'Templating issue' }
-	  	]);
+    configureRouter(config, router){
+        config.title = 'Grid - templating branch';
+        config.map([
+          { route: ['','basic-use'],    moduleId: './basic-use',         nav: true, title:'Basic use' },
+          { route: 'html-config',       moduleId: './html-config',       nav: true, title:'HTML configuration' },
+          { route: 'templating-issue',  moduleId: './templating-issue',  nav: true, title:'Templating issue' }
+          ]);
 
-	    this.router = router;
-	}
+        this.router = router;
+    }
 }
