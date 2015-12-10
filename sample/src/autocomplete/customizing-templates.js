70e12430cb4a6ec40b0e32997b9ae12d0ebad918
Jeroen <Jeroenvinke17@gmail.com>
2015-12-10 15:16:00 +0100

    feat(autocomplete): add template customization sample

--- sample/src/autocomplete/customizing-templates.js
+++ sample/src/autocomplete/customizing-templates.js
@@ -1,3 +1,21 @@
 export class CustomizingTemplates {
-    
+
+  template = '<span class="k-state-default" style="background-image: url(\'http://demos.telerik.com/kendo-ui/content/web/Customers/${data.CustomerID}.jpg\')"></span>' +
+              '<span class="k-state-default"><h3>${data.ContactName}</h3><p>${data.CompanyName}</p></span>';
+
+  headerTemplate = '<div class="dropdown-header k-widget k-header">' +
+                        '<span>Photo</span>' +
+                        '<span>Contact info</span>' +
+                    '</div>';
+
+  constructor() {
+    this.datasource = {
+      transport: {
+        read:{
+          dataType: "jsonp",
+          url: "//demos.telerik.com/kendo-ui/service/Customers"
+        }
+      }
+    };
+  }
 }
--- sample/src/autocomplete/remote-data.js
+++ sample/src/autocomplete/remote-data.js
@@ -1,13 +1,11 @@
 export class RemoteData {
-	datasource;
-
 	constructor() {
 		this.datasource = {
-                            type: "odata",
-                            serverFiltering: true,
-                            transport: {
-                                read: "//demos.telerik.com/kendo-ui/service/Northwind.svc/Products"
-                            }
-                        };
-	}	
+        type: "odata",
+        serverFiltering: true,
+        transport: {
+            read: "//demos.telerik.com/kendo-ui/service/Northwind.svc/Products"
+        }
+    };
+	}
 }
