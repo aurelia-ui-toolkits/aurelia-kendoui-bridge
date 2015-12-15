bf07274d897ad73d913f381b2d0bc5bd22505ee1
Jeroen <Jeroenvinke17@gmail.com>
2015-12-15 07:38:43 +0100

    fix(sample): smooth line

--- sample/src/line-charts/smooth-line.js
+++ sample/src/line-charts/smooth-line.js
@@ -2,8 +2,9 @@ export class SmoothLine {
   series = [{
       type: "line",
       data: [20, 1, 18, 3, 15, 5, 10, 6, 9, 6, 10, 5, 13, 3, 16, 1, 19, 1, 20, 2, 18, 5, 12, 7, 10, 8],
-      line: {
-          style: "smooth"
+      style: "smooth",
+      markers: {
+          visible: false
       }
   }];
 
