ccbe02c2fe788e6ae2c35b757f317429a99654d8
Jeroen <Jeroenvinke17@gmail.com>
2015-12-15 16:29:08 +0100

    refactor(nav-bar): add charts dropdown

--- sample/src/app.js
+++ sample/src/app.js
@@ -2,77 +2,43 @@ export class App {
     configureRouter(config, router) {
         config.title = 'Kendo UI Samples';
 
-        config.map([
-        {
+        config.map([{
+            name: 'about',
             route: ['','about'],
             moduleId: 'about/about',
-            nav: true,
             title: 'About'
         },  {
             route: 'installation/:page',
             moduleId: './installation-wizard/wizard',
             name: 'install',
             href: '#/installation/begin',
-            nav: true,
             title:'Installation wizard'
         },  {
+            name: 'autocomplete',
             route: 'autocomplete',
             moduleId: 'autocomplete/index',
-            nav: true,
             title: 'Autocomplete'
         },  {
+            name: 'button',
             route: 'button',
             moduleId: 'button/index',
-            nav: true,
             title: 'Button'
         },  {
+            name: 'area-charts',
             route: 'area-charts',
             moduleId: 'area-charts/index',
-            nav: true,
             title: 'Area Charts'
         },  {
+            name: 'line-charts',
             route: 'line-charts',
             moduleId: 'line-charts/index',
-            nav: true,
             title: 'Line Charts'
-        },
-        // },  {
-        //     route: 'combobox',
-        //     moduleId: 'combobox/index',
-        //     nav: true,
-        //     title: 'Combobox'
-        // },  {
-        //     route: 'dropdownlist',
-        //     moduleId: 'dropdownlist/index',
-        //     nav: true,
-        //     title: 'DropDownList'
-           {
+        },  {
+            name: 'grid',
             route: 'grid',
             moduleId: 'grid/index',
-            nav: true,
             title: 'Grid'
-        // },  {
-        //     route: 'scheduler',
-        //     moduleId: 'scheduler/index',
-        //     nav: true,
-        //     title: 'Scheduler'
-        // },  {
-        //     route: 'tabstrip',
-        //     moduleId: 'tabstrip/index',
-        //     nav: true,
-        //     title: 'TabStrip'
-        // },  {
-        //     route: 'menu',
-        //     moduleId: 'menu/index',
-        //     nav: true,
-        //     title: 'Menu'
-        // },  {
-        //     route: 'toolbar',
-        //     moduleId: 'toolbar/index',
-        //     nav: true,
-        //     title: 'Toolbar'
-        }
-        ]);
+        }]);
 
         this.router = router;
     }
--- sample/src/nav-bar.js
+++ sample/src/nav-bar.js
@@ -2,4 +2,5 @@ import {bindable} from 'aurelia-framework';
 
 export class NavBar {
   @bindable router = null;
+  chartRoutes = ['area-charts', 'line-charts'];
 }
