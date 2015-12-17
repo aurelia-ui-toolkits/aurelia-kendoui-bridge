a0c07151d54bedbbf978a26f0817e48d43c6de6a
Nik <nik@congral.com>
2015-12-16 21:15:25 -0500

    fix minor bugs in charts section
    fix: minor bugs in charts section
    
    minor bugs in charts section

--- sample/src/charts/line-charts/error-bars.js
+++ sample/src/charts/line-charts/error-bars.js
@@ -1,9 +1,18 @@
 export class ErrorBars {
-  series = [{
-      data: [7.943, 7.848, 9.284, 9.263, 9.801, 3.890, 8.238, 9.552],
+
+  seriesDefaults = {
+      type: "line",
       errorBars: {
-          value: "stddev"
+          value: "percentage(20)"
       }
+  };
+
+  series = [{
+      name: "India",
+      data: [3.907, 7.943, 7.848, 9.284, 9.263, 9.801, 3.890, 8.238, 9.552, 6.855]
+  }, {
+      name: "World",
+      data: [1.988, 2.733, 3.994, 3.464, 4.001, 3.939, 1.333, -2.245, 4.339, 2.727]
   }];
 
   valueAxis = {
@@ -17,15 +26,18 @@ export class ErrorBars {
   };
 
   categoryAxis = {
-      categories: [2003, 2004, 2005, 2006, 2007, 2008, 2009, 2010],
+      categories: [2002, 2003, 2004, 2005, 2006, 2007, 2008, 2009, 2010, 2011],
       line: {
-          visible: false
+              visible: false
+          },
+          labels: {
+              padding: { top: 80 }
       }
   };
 
   tooltip = {
       visible: true,
       format: "{0}%",
-      template: "${value} (σ = ${kendo.toString(high - low, 'N2')})"
+      template: "${series.name} ${value}"
   }
 }
--- sample/src/charts/line-charts/index.js
+++ sample/src/charts/line-charts/index.js
@@ -4,7 +4,7 @@ import {useView} from 'aurelia-framework';
 export class Index {
 
   configureRouter(config, router){
-      config.title = 'Line Chart';
+      config.title = 'Line Charts';
       config.map([
             { route: ['','basic-use'],            moduleId: './basic-use',                 nav: true,    title:'Basic use' },
             { route: 'stacked-line',              moduleId: './stacked-line',              nav: true,    title:'Stacked Line' },
