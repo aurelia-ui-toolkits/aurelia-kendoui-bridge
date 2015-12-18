24785a6355d3ac844adf11b0e66f46916f3d960a
Nik <nik@congral.com>
2015-12-18 18:18:07 -0500

    fix sample navigation
    fix(sample): fix sample navigation
    
    fix sample navigation

--- sample/src/nav-bar.js
+++ sample/src/nav-bar.js
@@ -2,6 +2,8 @@ import {bindable} from 'aurelia-framework';
 
 export class NavBar {
   @bindable router = null;
-  //chartRoutes = ['area-charts', 'bar-charts', 'box-plot-charts', 'bubble-charts', 'bullet-charts', 'donut-charts', 'funnel-charts', 'line-charts', 'pie-charts'];
-  chartRoutes = ['area-charts', 'bar-charts', 'box-plot-charts', 'bubble-charts', 'line-charts'];
+  chartRoutes = [
+                  'area-charts', 'bar-charts', 'box-plot-charts', 'bubble-charts', 'bullet-charts', 'donut-charts',   'funnel-charts',
+                  'line-charts', 'pie-charts', 'polar-charts',    'radar-charts',  'range-charts',  'scatter-charts', 'waterfall-charts'
+                ];
 }
