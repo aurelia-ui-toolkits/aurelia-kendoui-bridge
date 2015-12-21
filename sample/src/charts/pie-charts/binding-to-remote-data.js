a179653cbebb1e395c2b444c6892672d13c8ee98
Nik <nik@congral.com>
2015-12-20 21:40:54 -0500

    add complete pie charts
    feat(sample): add complete pie charts
    
    add complete pie charts

--- sample/src/charts/pie-charts/binding-to-remote-data.js
+++ sample/src/charts/pie-charts/binding-to-remote-data.js
@@ -1,38 +1,41 @@
 export class BindingToRemoteData {
-  datasource = new kendo.data.DataSource({
-    transport: {
-      read: function(options) {
-        return System.import('charts/line-charts/json/spain-electricity.json!json')
-        .then(data => options.success(data));
-      }
-    },
-    sort: {
-        field: "year",
-        dir: "asc"
-    }
-  });
+    dataSource = new kendo.data.DataSource({
+        transport: {
+          read: function(options) {
+            return System.import('charts/pie-charts/json/screen_resolution.json!json')
+            .then(data => options.success(data));
+          }
+        },
+        filter: {
+            field: "year", operator: "eq", value: 2000
+        },
+        sort: {
+            field: "year",
+            dir: "asc"
+        }
+    });
+
+    title = {
+        text: 2000
+    };
 
     seriesDefaults = {
-
+        type: "pie"
     };
 
     legend = {
-        visible: false
-    }
+        position: "top"
+    };
 
     series = [{
-
+        field: "share",
+        categoryField: "resolution",
+        padding: 0
     }];
 
-    valueAxis = {
-
-    };
-
-    categoryAxis = {
-
-    };
-
     tooltip = {
-
+        visible: true,
+        format: "N0",
+        template: "#= category # - #= kendo.format('{0:P}', percentage)#"
     }
 }
---
+++ sample/src/charts/pie-charts/pie-labels.js
@@ -0,0 +1,46 @@
+export class PieLabels {
+    seriesDefaults = {
+        labels: {
+            template: "#= category # - #= kendo.format('{0:P}', percentage)#",
+            position: "outsideEnd",
+            visible: true,
+            background: "transparent"
+        }
+    };
+
+    legend = {
+        position: "top"
+    }
+
+    series = [{
+        type: "pie",
+        data: [{
+            category: "Football",
+            value: 35
+        }, {
+            category: "Basketball",
+            value: 25
+        }, {
+            category: "Volleyball",
+            value: 20
+        }, {
+            category: "Rugby",
+            value: 10
+        }, {
+            category: "Tennis",
+            value: 10
+        }]
+    }];
+
+    valueAxis = {
+
+    };
+
+    categoryAxis = {
+
+    };
+
+    tooltip = {
+
+    }
+}
