899abd59c4a38ad8c6ef0aadbec7cfaea66260fb
Charleh <charles.pockert@googlemail.com>
2015-12-22 01:08:44 +0000

    feat(all): implement dropdownlist, progressbar, slider + samples

---
+++ src/dropdownlist/dropdownlist.js
@@ -0,0 +1,82 @@
+import {customAttribute, bindable, inject} from 'aurelia-framework';
+import {fireEvent, WidgetBase, TemplateCompiler, generateBindables} from '../common/index';
+import 'jquery';
+import 'kendo-ui/js/kendo.dropdownlist.min';
+import 'kendo-ui/js/kendo.virtuallist.min';
+
+@customAttribute('au-kendo-drop-down-list')
+@inject(Element, TemplateCompiler)
+@generateBindables('kendoDropDownList')
+export class AuKendoDropDownList extends WidgetBase {
+
+  @bindable options = {};
+  @bindable dataSource;
+  @bindable value;
+
+  constructor(element, templateCompiler) {
+    super('kendoDropDownList', element);
+
+    this.templateCompiler = templateCompiler;
+  }
+ 
+  bind(ctx) {
+    this.templateCompiler.initialize(ctx);
+
+    this._initialize();
+  }
+
+  recreate() {
+    this._initialize();
+  }
+
+  _initialize() {
+    super._initialize();
+
+    // without these change and select handlers, when you select an options
+    // the value binding is not updated
+    this.widget.bind('change', (event) => {
+      this.value = event.sender.value();
+
+      // Update the kendo binding
+      fireEvent(this.element, 'input');
+    });
+
+    this.widget.bind('select', (event) => {
+      this.value = event.sender.value();
+
+      // Update the kendo binding
+      fireEvent(this.element, 'input');
+    });
+  }
+
+  enableChanged(newValue) {
+    if (this.widget) {
+      this.widget.enable(newValue);
+    }
+  }
+
+  setValue(newValue) {
+    if (this.widget) {
+      this.widget.value(newValue);
+      this.widget.trigger('change');
+    }
+  }
+
+  getValue(newValue) {
+    if (this.widget) {
+      return this.widget.value();
+    }
+  }
+
+  search(value) {
+    if (this.widget) {
+      this.widget.search(value);
+    }
+  }
+
+  detached() {
+    if (this.widget) {
+      this.widget.destroy();
+    }
+  }
+}
