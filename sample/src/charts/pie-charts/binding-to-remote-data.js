9c92d794a4d3d33ea2fdbbceae257ccbe5ce3dc1
Jeroen <Jeroenvinke17@gmail.com>
2015-12-23 18:23:16 +0100

    fix(sample): fix pie chart remote data
    https://github.com/aurelia-ui-toolkits/aurelia-kendoui-plugin/issues/185

--- sample/src/charts/pie-charts/binding-to-remote-data.js
+++ sample/src/charts/pie-charts/binding-to-remote-data.js
@@ -1,19 +1,4 @@
 export class BindingToRemoteData {
-    dataSource = new kendo.data.DataSource({
-        transport: {
-          read: function(options) {
-            return System.import('charts/pie-charts/json/screen_resolution.json!json')
-            .then(data => options.success(data));
-          }
-        },
-        filter: {
-            field: "year", operator: "eq", value: 2000
-        },
-        sort: {
-            field: "year",
-            dir: "asc"
-        }
-    });
 
     title = {
         text: 2000
@@ -38,5 +23,23 @@ export class BindingToRemoteData {
         visible: true,
         format: "N0",
         template: "#= category # - #= kendo.format('{0:P}', percentage)#"
+    }
+
+    getDatasource(year) {
+      return new kendo.data.DataSource({
+          transport: {
+            read: function(options) {
+              return System.import('charts/pie-charts/json/screen_resolution.json!json')
+              .then(data => options.success(data));
+            }
+          },
+          filter: {
+              field: "year", operator: "eq", value: year
+          },
+          sort: {
+              field: "year",
+              dir: "asc"
+          }
+      });
     }
 }
