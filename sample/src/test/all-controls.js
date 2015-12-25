fed21334b6b04ec371d60c1ad6e6b884e4a2c176
Jeroen <Jeroenvinke17@gmail.com>
2015-12-25 18:05:37 +0100

    update eslint

---
+++ sample/src/test/all-controls.js
@@ -0,0 +1,26 @@
+export class AllControls {
+  samples = [{
+    name: 'Button',
+    path: 'button/basic-use'
+  }, {
+    name: 'Events test',
+    path: 'button/api',
+    description: 'When you click on "Click me", you should get an alert. This indicates that events are fired correctly'
+  }, {
+    name: 'Autocomplete',
+    path: 'autocomplete/basic-use'
+  }, {
+    name: 'Dropdownlist',
+    path: 'dropdownlist/basic-use'
+  }, {
+    name: 'Grid',
+    path: 'grid/basic-use',
+    description: 'Pictures should be shown, as this is renderd via our templating engine'
+  }, {
+    name: 'Progressbar',
+    path: 'progressbar/basic-use'
+  }, {
+    name: 'Slider',
+    path: 'slider/basic-use'
+  }];
+}
