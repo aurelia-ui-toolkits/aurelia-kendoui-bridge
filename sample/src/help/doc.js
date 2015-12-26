7ce414e7ebe46331451dfe9339db69f4646fd461
Jeroen <Jeroenvinke17@gmail.com>
2015-12-26 10:20:34 +0100

    doc panelbar control

--- sample/src/help/doc.js
+++ sample/src/help/doc.js
@@ -1,7 +1,20 @@
 import registry from './registry.json!';
+import 'kendo-ui/js/kendo.panelbar.min';
 
 export class Doc {
   constructor() {
     this.registry = registry;
+
+    this.categories = [];
+    for (let category of Object.keys(this.registry)) {
+      this.categories.push({
+        name: category,
+        files: this.registry[category]
+      });
+    }
+  }
+
+  attached() {
+    $(this.panelbar).kendoPanelBar();
   }
 }
