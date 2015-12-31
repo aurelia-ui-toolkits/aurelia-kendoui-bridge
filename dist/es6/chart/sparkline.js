b2c97caac486bf26bca8a39de17e822851d97493
Nik <nik@congral.com>
2015-12-30 19:02:38 -0500

    rename charts folder to chart for consistency
    fix(sample): rename charts folder to chart for consistency
    
    rename charts folder to chart for consistency (all other control types
    use singular form)

--- dist/es6/chart/sparkline.js
+++ dist/es6/chart/sparkline.js
@@ -1,10 +1,9 @@
-import {customElement, noView, bindable, inject} from 'aurelia-framework';
+import {customElement, bindable, inject} from 'aurelia-framework';
 import {WidgetBase, generateBindables} from '../common/index';
 import '../pdf/pdf';
 import 'kendo-ui/js/kendo.dataviz.sparkline.min';
 
 @customElement('k-sparkline')
-@noView
 @generateBindables('kendoSparkline')
 @inject(Element)
 export class Sparkline extends WidgetBase {
