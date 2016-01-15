e0bddb05c29aea85701746abd53dfd967a071b08
Jeroen <Jeroenvinke17@gmail.com>
2016-01-15 19:56:59 +0100

    feat(installation): linkable pages
    fixes https://github.com/aurelia-ui-toolkits/aurelia-kendoui-plugin/issues/245

--- sample/src/app.js
+++ sample/src/app.js
@@ -7,7 +7,7 @@ export class App {
       { name: 'help',             route: 'help',                       moduleId: 'help/help',                   title: 'Help' },
       { name: 'docs',             route: 'help/docs/:category/:file',  moduleId: 'help/help',                   title: 'Help' },
       { name: 'home',             route: '',                           redirect: 'samples' },
-      { name: 'installation',     route: 'installation',               moduleId: 'installation/index',          title: 'Installation' },
+      { name: 'installation',     route: 'installation/:type',         moduleId: 'installation/index',          title: 'Installation' },
       { name: 'project-status',   route: 'project-status',             moduleId: 'project-status/controls',     title: 'Project status'},
       { name: 'samples',          route: 'samples',                    moduleId: 'samples/index',               title: 'Samples' },
       { name: 'test',             route: 'test',                       moduleId: 'test/index',                  title: 'Test pages' }
--- sample/src/installation/index.js
+++ sample/src/installation/index.js
@@ -1,1 +1,26 @@
-export class Index {}
+import {inject} from 'aurelia-framework';
+import {Router} from 'aurelia-router';
+
+@inject(Router)
+export class Index {
+
+  constructor(router) {
+    this.router = router;
+  }
+
+  activate(params) {
+    this.params = params;
+  }
+
+  attached() {
+    let type = this.params.type;
+
+    this.tabstrip.widget.select(this[type]);
+  }
+
+  tabChanged(e) {
+    this.router.navigateToRoute('installation', {
+      type: $(e.item).data('type')
+    });
+  }
+}
