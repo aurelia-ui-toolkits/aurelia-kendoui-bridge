918cf56da81c5aa6ed35a8bec54a3dc59339f191
Nik <nik@congral.com>
2016-01-17 18:05:49 -0500

    add treeview to project status control

--- sample/src/samples/index.js
+++ sample/src/samples/index.js
@@ -15,7 +15,6 @@ export class Index {
       { name: 'default',          route: '',                    redirect: 'grid' },
       { name: 'grid',             route: 'grid',                moduleId: './grid/index',                    title: 'Grid'},
       { name: 'autocomplete',     route: 'autocomplete',        moduleId: './autocomplete/index',            title: 'Autocomplete'},
-      { name: 'treeview',         route: 'treeview',            moduleId: './treeview/index',               title: 'Treeview'},
       { name: 'area-charts',      route: 'area-charts',         moduleId: './chart/area-charts/index',      title: 'Area Charts'},
       { name: 'bar-charts',       route: 'bar-charts',          moduleId: './chart/bar-charts/index',       title: 'Bar Charts'},
       { name: 'box-plot-charts',  route: 'box-plot-charts',     moduleId: './chart/box-plot-charts/index',  title: 'Box Plot Charts'},
@@ -37,6 +36,7 @@ export class Index {
       { name: 'stock',            route: 'stock',               moduleId: './chart/stock/index',            title: 'Stock'},
       { name: 'tabstrip',         route: 'tabstrip',            moduleId: './tabstrip/index',                title: 'Tabstrip'},
       { name: 'treemap',          route: 'treemap',             moduleId: './chart/treemap/index',          title: 'Treemap'},
+      { name: 'treeview',         route: 'treeview',            moduleId: './treeview/index',               title: 'Treeview'},
       { name: 'waterfall-charts', route: 'waterfall-charts',    moduleId: './chart/waterfall-charts/index', title: 'Waterfall Charts'}
     ]);
 
