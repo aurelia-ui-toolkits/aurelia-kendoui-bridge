07e3e9fc2680c8efe1be14b514352a177c31f014
Charleh <charles.pockert@googlemail.com>
2016-01-17 22:50:54 +0000

    feat(build): added dev build with gitignored build folder release build is now `build-release`

--- build/tasks/build.js
+++ build/tasks/build.js
@@ -98,3 +98,16 @@ gulp.task('build', function(callback) {
     callback
   );
 });
+
+gulp.task('build-release', function(callback) {
+  paths.output = paths.releaseOutput;
+  
+  return runSequence(
+    'clean',
+    'build-index',
+    ['build-es6-temp', 'build-es6', 'build-commonjs', 'build-amd', 'build-system'],
+    ['copy-html', 'copy-css'],
+    'build-dts',
+    callback
+  );
+});
