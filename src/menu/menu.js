7817050fd0f876571384fe0817ee3ce73b4335b0
Jeroen <Jeroenvinke17@gmail.com>
2016-01-28 20:46:49 +0100

    refactor(all): use constants to create custom element/attribute
    closes https://github.com/aurelia-ui-toolkits/aurelia-kendoui-bridge/issues/279

--- src/menu/menu.js
+++ src/menu/menu.js
@@ -2,9 +2,10 @@ import {inject} from 'aurelia-dependency-injection';
 import {customElement, bindable} from 'aurelia-templating';
 import {WidgetBase} from '../common/widget-base';
 import {generateBindables} from '../common/decorators';
+import {constants} from '../common/constants';
 import 'kendo-ui/js/kendo.menu.min';
 
-@customElement('k-menu')
+@customElement(`${constants.elementPrefix}menu`)
 @generateBindables('kendoMenu')
 @inject(Element, WidgetBase)
 export class Menu {
--- src/progressbar/progressbar.js
+++ src/progressbar/progressbar.js
@@ -2,9 +2,10 @@ import {inject} from 'aurelia-dependency-injection';
 import {customAttribute, bindable} from 'aurelia-templating';
 import {WidgetBase} from '../common/widget-base';
 import {generateBindables} from '../common/decorators';
+import {constants} from '../common/constants';
 import 'kendo-ui/js/kendo.progressbar.min';
 
-@customAttribute('k-progress-bar')
+@customAttribute(`${constants.attributePrefix}progress-bar`)
 @generateBindables('kendoProgressBar')
 @inject(Element, WidgetBase)
 export class ProgressBar {
