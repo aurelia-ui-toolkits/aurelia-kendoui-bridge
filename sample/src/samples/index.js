82496838bd24a20b36ed8cced63e47f140dd9ce7
Charleh <charles.pockert@googlemail.com>
2016-01-29 21:28:18 +0000

    chore(sample): added some ColorPicker/ColorPalette sample pages

--- sample/src/samples/index.js
+++ sample/src/samples/index.js
@@ -40,7 +40,8 @@ export class Index {
       { name: 'treemap',          route: 'treemap',             moduleId: './chart/treemap/index',          title: 'Treemap'},
       { name: 'treeview',         route: 'treeview',            moduleId: './treeview/index',               title: 'Treeview'},
       { name: 'waterfall-charts', route: 'waterfall-charts',    moduleId: './chart/waterfall-charts/index', title: 'Waterfall Charts'},
-      { name: 'panelbar',         route: 'panelbar',            moduleId: './panelbar/index',               title: 'Panel Bar'}
+      { name: 'panelbar',         route: 'panelbar',            moduleId: './panelbar/index',               title: 'Panel Bar'},
+      { name: 'colorpicker',      route: 'colorpicker',         moduleId: './colorpicker/index',            title: 'Color Picker'}
     ]);
 
     this.router = router;
