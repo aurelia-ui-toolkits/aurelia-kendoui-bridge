801484e43ccee17bc4e2e44437d7f6d03f4b21bb
Jeroen <Jeroenvinke17@gmail.com>
2016-02-06 10:01:26 +0100

    chore(samples): dropdownlist

--- sample/src/samples/dropdownlist/grouping.js
+++ sample/src/samples/dropdownlist/grouping.js
@@ -1,1 +1,9 @@
-export class Grouping {}
+export class Grouping {
+  dataSource = {
+    type: 'odata',
+    transport: {
+      read: '//demos.telerik.com/kendo-ui/service/Northwind.svc/Customers'
+    },
+    group: { field: 'Country' }
+  }
+}
