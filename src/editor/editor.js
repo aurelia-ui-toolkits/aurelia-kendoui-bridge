6755035aab69764edad1ed403c20621471e4a756
Nik <nik@congral.com>
2016-02-09 19:01:48 -0500

    chore(editor): implement editor via custom attribute k-editor

--- src/editor/editor.js
+++ src/editor/editor.js
@@ -1,37 +1,34 @@
 import {inject} from 'aurelia-dependency-injection';
-import {customElement, bindable} from 'aurelia-templating';
+import {customAttribute, bindable} from 'aurelia-templating';
 import {WidgetBase} from '../common/widget-base';
 import {generateBindables} from '../common/decorators';
 import {constants} from '../common/constants';
 import 'kendo-ui/js/kendo.editor.min';
 
-@customElement(`${constants.elementPrefix}editor`)
+@customAttribute(`${constants.attributePrefix}editor`)
 @generateBindables('kendoEditor')
 @inject(Element, WidgetBase)
 export class Editor {
 
   @bindable options = {};
 
-  constructor(element, widgetBase, viewResources) {
+  constructor(element, widgetBase) {
     this.element = element;
     this.widgetBase = widgetBase
-                    .control('kendoEditor')
-                    .linkViewModel(this)
-                    .useValueBinding();
+                        .control('kendoEditor')
+                        .linkViewModel(this)
+                        .useValueBinding();
   }
 
   bind(ctx) {
     this.$parent = ctx;
-  }
 
-  attached() {
     this.recreate();
   }
 
   recreate() {
     this.kWidget = this.widgetBase.createWidget({
-      element: this.textarea,
-      rootElement: this.element,
+      element: this.element,
       parentCtx: this.$parent
     });
   }
