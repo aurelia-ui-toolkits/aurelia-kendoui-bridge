d4195ee62d825cf6559aff411063b167ab071a99
Jeroen <Jeroenvinke17@gmail.com>
2016-02-16 19:35:47 +0100

    chore(editor): specific editor import

--- src/editor/editor.js
+++ src/editor/editor.js
@@ -3,7 +3,7 @@ import {customAttribute, bindable} from 'aurelia-templating';
 import {WidgetBase} from '../common/widget-base';
 import {generateBindables} from '../common/decorators';
 import {constants} from '../common/constants';
-import 'kendo-ui/js/kendo.all.min';
+import 'kendo-ui/js/kendo.editor.min';
 import 'kendo-ui/js/jszip.min.js';
 
 import 'kendo-ui/js/pako_deflate.min.js';
