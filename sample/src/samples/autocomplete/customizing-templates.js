8b8ca83f22226aa67728879f1299f0dcec23a5ff
Jeroen <Jeroenvinke17@gmail.com>
2016-02-17 17:05:19 +0100

    chore(sample): css improvements

--- sample/src/samples/autocomplete/customizing-templates.js
+++ sample/src/samples/autocomplete/customizing-templates.js
@@ -1,7 +1,7 @@
 export class CustomizingTemplates {
 
-  template = '<span class="k-state-default" style="background-image: url(\'http://demos.telerik.com/kendo-ui/content/web/Customers/${data.CustomerID}.jpg\')"></span>' +
-              '<span class="k-state-default"><h3>${data.ContactName}</h3><p>${data.CompanyName}</p></span>';
+  template = '<span class="k-state-default" style="background-image: url(\'http://demos.telerik.com/kendo-ui/content/web/Customers/${CustomerID}.jpg\')"></span>' +
+              '<span class="k-state-default"><h3>${ContactName}</h3><p>${CompanyName}</p></span>';
 
   headerTemplate = '<div class="dropdown-header k-widget k-header">' +
                         '<span>Photo</span>' +
