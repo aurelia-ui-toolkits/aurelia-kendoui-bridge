e12b6034fc3dc5f81b06af307424a16fc63e9ab1
Jeroen <Jeroenvinke17@gmail.com>
2016-02-17 17:15:41 +0100

    chore(samples): menubar

--- src/menu/menu.js
+++ src/menu/menu.js
@@ -27,11 +27,12 @@ export class Menu {
 
   recreate() {
     let element;
-    let ul = $(this.element).find('ul');
-    if (ul.has()) {
-      element = $(this.element).find('ul').first();
+    let ul = this.element.querySelectorAll('ul');
+    if (ul.length > 0) {
+      element = ul[0];
     } else {
-      element = $(this.element).appendChild('<ul></ul>');
+      element = document.createElement('ul');
+      this.element.appendChild(element);
     }
 
     this.kWidget = this.widgetBase.createWidget({
