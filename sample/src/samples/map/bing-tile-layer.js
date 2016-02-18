7cab83af1d36ecfe02a3a84950605af1bf147f3c
Nik <nik@congral.com>
2016-02-18 17:10:05 -0500

    chore(all): add documents, fixes for typps

--- sample/src/samples/map/bing-tile-layer.js
+++ sample/src/samples/map/bing-tile-layer.js
@@ -5,7 +5,11 @@ center = [51.505, -0.09];
 layers = [{
       type: "bing",
       imagerySet: "aerialWithLabels",
-       key: "AqaPuZWytKRUA8Nm5nqvXHWGL8BDCXvK8onCl2PkC581Zp3T_fYAQBiwIphJbRAK"
+      /*
+       *  IMPORTANT: This key is locked to demos.telerik.com
+       *  Please replace with your own Bing Key
+       */
+      key: "AqaPuZWytKRUA8Nm5nqvXHWGL8BDCXvK8onCl2PkC581Zp3T_fYAQBiwIphJbRAK"
   }];
 
 }
