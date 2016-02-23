1ad1fd097d89a174812ac0bf381a6f3c6ed3ab5c
Jeroen <Jeroenvinke17@gmail.com>
2016-02-23 18:31:02 +0100

    chore(sample): convert installation section to child router
    closes https://github.com/aurelia-ui-toolkits/aurelia-kendoui-bridge/issues/370

--- sample/src/installation/index.js
+++ sample/src/installation/index.js
@@ -1,26 +1,10 @@
-import {inject} from 'aurelia-framework';
-import {Router} from 'aurelia-router';
-
-@inject(Router)
 export class Index {
-
-  constructor(router) {
-    this.router = router;
-  }
-
-  activate(params) {
-    this.params = params;
-  }
-
-  attached() {
-    let type = this.params.type;
-
-    this.tabstrip.kWidget.select(this[type]);
-  }
-
-  tabChanged(e) {
-    this.router.navigateToRoute('installation', {
-      type: $(e.item).data('type')
-    });
+  configureRouter(config, router) {
+    config.map([
+      { name: 'default',    route: ['installation', ''],  moduleId: './instructions' },
+      { name: 'core',       route: 'core',               moduleId: './instructions',           title: 'Core'},
+      { name: 'jspmGit',    route: 'jspm-git',           moduleId: './instructions',           title: 'jspm-git'},
+      { name: 'vendors',    route: 'vendors',            moduleId: './instructions',           title: 'Vendors'}
+    ]);
   }
 }
