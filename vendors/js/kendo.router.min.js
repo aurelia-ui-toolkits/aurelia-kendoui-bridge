fecc8fe6fb2b124b26332d154014e4ffc0328175
Jeroen <Jeroenvinke17@gmail.com>
2016-02-29 20:34:03 +0100

    chore(kendo): update kendo

--- vendors/js/kendo.router.min.js
+++ vendors/js/kendo.router.min.js
@@ -1,5 +1,5 @@
 /** 
- * Kendo UI v2016.1.118 (http://www.telerik.com/kendo-ui)                                                                                                                                               
+ * Kendo UI v2016.1.226 (http://www.telerik.com/kendo-ui)                                                                                                                                               
  * Copyright 2016 Telerik AD. All rights reserved.                                                                                                                                                      
  *                                                                                                                                                                                                      
  * Kendo UI commercial licenses may be obtained at                                                                                                                                                      
