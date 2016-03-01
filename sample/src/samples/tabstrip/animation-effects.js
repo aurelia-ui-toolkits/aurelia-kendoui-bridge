d12ca6aa143f271a7fff9f139898e1161d178cac
Jeroen <Jeroenvinke17@gmail.com>
2016-03-01 08:58:06 +0100

    feat(tabstrip): tabstrip wrapper

--- sample/src/samples/tabstrip/animation-effects.js
+++ sample/src/samples/tabstrip/animation-effects.js
@@ -1,1 +1,35 @@
-export class AnimationEffects {}
+export class AnimationEffects {
+  animation = 'expand:vertical';
+  opacity = true;
+
+  getEffects() {
+    if (!this.animation && !this.opacity) {
+      return false;
+    }
+
+    let anim = '';
+    if (this.opacity) {
+      anim += 'fadeIn';
+    }
+
+    if (this.animation) {
+      anim += ' ' + this.animation;
+    }
+    console.log(anim);
+    return anim;
+  }
+
+  attached() {
+    $('input').change(() => {
+      this.tabstripVM.kAnimation = {
+        open: {
+          effects: this.getEffects()
+        }
+      };
+
+      console.log(this);
+
+      this.tabstripVM.recreate();
+    });
+  }
+}
--- sample/src/samples/tabstrip/api.js
+++ sample/src/samples/tabstrip/api.js
@@ -1,1 +1,49 @@
-export class API {}
+export class API {
+  beforeText = 'Item';
+  afterText = 'Item';
+  appendText = 'Item';
+  tabIndex = '0';
+
+  getItem(index) {
+    return this.tabStrip.tabGroup.children('li').eq(index);
+  }
+
+  selectTab() {
+    this.tabStrip.select(this.getItem(this.tabIndex));
+  }
+
+  toggleTab() {
+    let tab = this.tabStrip.select();
+    this.tabStrip.enable(tab, tab.hasClass('k-state-disabled'));
+  }
+
+  removeItem() {
+    let tab = this.tabStrip.select();
+    let otherTab = tab.next();
+    otherTab = otherTab.length ? otherTab : tab.prev();
+
+    this.tabStrip.remove(tab);
+    this.tabStrip.select(otherTab);
+  }
+
+  appendItem() {
+    this.tabStrip.append({
+      text: this.appendText,
+      content: '<br>'
+    });
+  }
+
+  beforeItem() {
+    this.tabStrip.insertBefore({
+      text: this.beforeText,
+      content: '<br>'
+    }, this.tabStrip.select());
+  }
+
+  afterItem() {
+    this.tabStrip.insertAfter({
+      text: this.afterText,
+      content: '<br>'
+    }, this.tabStrip.select());
+  }
+}
