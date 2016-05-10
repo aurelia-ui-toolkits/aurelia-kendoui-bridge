339cf9858681ba83a1f9cd052e886db81e71a0fc
Jeroen <Jeroenvinke17@gmail.com>
2016-05-10 07:48:20 +0200

    chore(sample): make help tabs routable
    closes https://github.com/aurelia-ui-toolkits/aurelia-kendoui-bridge/issues/486#issuecomment-218040285

--- sample/src/help/help.js
+++ sample/src/help/help.js
@@ -1,1 +1,28 @@
-export class Help {}
+import {inject, TaskQueue} from 'aurelia-framework';
+import {Router} from 'aurelia-router';
+
+@inject(Router, TaskQueue)
+export class Help {
+
+  constructor(router, taskqueue) {
+    this.router = router;
+    this.taskqueue = taskqueue;
+  }
+
+  activate(params, route) {
+    this.route = route;
+  }
+
+  attached() {
+    let tab = this.route.route;
+
+    this.taskqueue.queueTask(() => {
+      console.log(tab);
+      this.tabstrip.select(this[tab]);
+    });
+  }
+
+  tabSelected(e) {
+    this.router.navigateToRoute($(e.item).data('tab'));
+  }
+}
