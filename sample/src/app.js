072bf68deefbb8bcde6baf5ff78d6612e570ac58
adriatic <nik@congral.com>
2016-05-14 18:36:43 -0400

    chore(catalog): improved help/support tab

--- sample/src/app.js
+++ sample/src/app.js
@@ -5,7 +5,6 @@ export class App {
     config.map([
       { name: 'about',            route: 'about',                      moduleId: 'about/about',                 title: 'About' },
       { name: 'help',             route: 'help',                       moduleId: 'help/index',                  title: 'Help' },
-      //{ name: 'docs',             route: 'help/docs/:category/:file',  moduleId: 'help/help',                   title: 'Help' },
       { name: 'home',             route: '',                           redirect: 'about/about' },
       { name: 'installation',     route: 'installation',               moduleId: 'installation/index',          title: 'Installation' },
       { name: 'catalog-index',    route: 'catalog-index',              moduleId: 'catalog-index/controls',      title: 'Catalog-index'},
