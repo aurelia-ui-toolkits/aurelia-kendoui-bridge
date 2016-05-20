3c16124558bcd36c6ca7606bf551569c9d04f251
adriatic <nik@congral.com>
2016-05-19 23:15:10 -0400

    feat(support): add basic info on support exchange service

--- sample/src/help/index.js
+++ sample/src/help/index.js
@@ -1,9 +1,11 @@
 export class Index {
   configureRouter(config, router) {
     config.map([
-      { name: 'default',    route: ['help', ''],         moduleId: './help' },
-      { name: 'docs',       route: 'docs',               moduleId: './help',           title: 'Component Docs'},
-      { name: 'support',    route: 'support',            moduleId: './help',           title: 'Support'}
+      { name: 'default',           route: ['help', ''],         moduleId: './help' },
+      { name: 'docs',              route: 'docs',               moduleId: './help',   title: 'Component Docs'},
+      { name: 'free-support',      route: 'free-support',       moduleId: './help',   title: 'Support'},
+      { name: 'support-exchange',  route: 'support-exchange',   moduleId: './help',   title: 'Support Exchange'}
+
     ]);
   }
 }
