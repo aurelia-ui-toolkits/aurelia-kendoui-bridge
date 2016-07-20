4a60f4a1562a57c0e05ea8664f500d5f380fabbc
Jeroen <Jeroenvinke17@gmail.com>
2016-07-20 15:59:03 +0200

    chore(sample): get samples from gists

--- sample/src/sample-runner.js
+++ sample/src/sample-runner.js
@@ -2,14 +2,17 @@ import {inject, bindable} from 'aurelia-framework';
 import {activationStrategy} from 'aurelia-router';
 import {EventAggregator} from 'aurelia-event-aggregator';
 import {TaskQueue} from 'aurelia-framework';
+import {HttpClient} from 'aurelia-http-client';
+import {Settings} from './settings';
 
-@inject(EventAggregator, TaskQueue)
+@inject(EventAggregator, TaskQueue, Settings)
 export class SampleRunner {
 
   @bindable router;
 
-  constructor(ea, taskQueue) {
+  constructor(ea, taskQueue, settings) {
     this.taskQueue = taskQueue;
+    this.settings = settings;
     this.ea = ea;
   }
 
@@ -17,12 +20,34 @@ export class SampleRunner {
     let sample = route.navModel.config.sample;
 
     if (!sample) throw new Error('Route does not contain a \'sample\' property');
+    if (!sample.gist) throw new Error('sample does not have a gist');
+
+    // app.js is the view-model of the gist
+    // aurelia's <compose> automatically loads app.html
+    sample.path = `${this.settings.gistProxy}file/${sample.gist}/app`;
+
+    // we need to get all files from the gist to show it in
+    // the code preview
+    new HttpClient()
+    .configure(x => {
+      x.withBaseUrl(this.settings.gistProxy);
+    })
+    .get(`files/${sample.gist}`)
+    .then(response => {
+      let files = response.content;
+      let keys = Object.keys(files);
+      if (keys.indexOf('app.html') > -1) {
+        sample.html = files['app.html'].content;
+      }
+      if (keys.indexOf('app.js') > -1) {
+        sample.js = files['app.js'].content;
+      }
+      if (keys.indexOf('app.css') > -1) {
+        sample.css = files['app.css'].content;
+      }
+    });
 
     this.sample = sample;
-
-    if (this.sample.gist) {
-      this.sample.gist = `https://gist.run/?id=${this.sample.gist}`;
-    }
 
     this.unsubscribe = this.ea.subscribe('kendo:skinChange', () => this.restart());
   }
--- sample/src/settings.js
+++ sample/src/settings.js
@@ -1,4 +1,5 @@
 export class Settings {
   defaultTheme = 'default';
   activeTheme = 'default';
+  gistProxy = 'http://gist-serve.jeroenvinke.nl/';
 }
