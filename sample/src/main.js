bcd5b23884696bbfe593df6e0569b8be28b6a302
Jeroen <Jeroenvinke17@gmail.com>
2016-07-31 10:28:09 +0200

    chore(sample): enabled debug mode for catalog

--- sample/src/main.js
+++ sample/src/main.js
@@ -6,7 +6,7 @@ export function configure(aurelia) {
   aurelia.use
     .standardConfiguration()
     .developmentLogging()
-    .plugin('aurelia-kendoui-bridge', (kendo) => kendo.detect().notifyBindingBehavior());
+    .plugin('aurelia-kendoui-bridge', (kendo) => kendo.detect().notifyBindingBehavior().debug());
 
   aurelia.use.globalResources('shared/collapse-panel');
   aurelia.use.globalResources('shared/markdown');
