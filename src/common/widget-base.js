299edc3885eaf8a0e0e2c7d7c879d4e2b99995a8
JeroenVinke <jeroenvinke17@gmail.com>
2016-10-04 16:49:56 +0200

    fix(widget-base): change binding value only when needed
    fixes https://github.com/aurelia-ui-toolkits/aurelia-kendoui-bridge/issues/642

--- src/common/widget-base.js
+++ src/common/widget-base.js
@@ -272,7 +272,9 @@ export class WidgetBase {
   handlePropertyChanged(widget, property, newValue, oldValue) {
     let binding = this.bindingsToKendo.find(i => i.propertyName === property);
     if (binding) {
-      widget[binding.functionName](newValue);
+        if (widget && widget[binding.functionName]() !== newValue) {
+          widget[binding.functionName](newValue);
+        }
     }
   }
 
