ff6b462009dad67a4b1adf4cc3452c076c44bf56
JeroenVinke <jeroenvinke17@gmail.com>
2016-10-04 16:49:56 +0200

    fix(widget-base): change binding value only when needed
    fixes https://github.com/aurelia-ui-toolkits/aurelia-kendoui-bridge/issues/642

--- src/common/widget-base.js
+++ src/common/widget-base.js
@@ -279,7 +279,9 @@ export class WidgetBase {
     if (typeof newValue === 'undefined') {
       widget[binding.functionName](null);
     } else {
-      widget[binding.functionName](newValue);
+      if (widget && widget[binding.functionName]() !== newValue) {
+        widget[binding.functionName](newValue);
+      }
     }
   }
 
