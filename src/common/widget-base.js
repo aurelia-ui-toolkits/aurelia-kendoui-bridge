3d01e659174952a5401955a8733a98b6dc1e8562
JeroenVinke <jeroenvinke17@gmail.com>
2017-03-21 09:19:27 +0100

    fix(widget-base): destroy element before emptying it

--- src/common/widget-base.js
+++ src/common/widget-base.js
@@ -337,7 +337,7 @@ export class WidgetBase {
   * destroys the widget
   */
   destroy(widget) {
-    if (widget && widget.element.length > 0) {
+    if (widget && widget.element && widget.element.length > 0) {
       if (widget.wrapper && (widget.wrapper !== widget.element)) {
         widget.element.insertBefore(widget.wrapper);
         widget.wrapper.remove();
