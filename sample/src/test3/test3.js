a6eb965e7e04ce441e7f4d8cbd90c46285f787c8
pmarchio <pgmarchione@gmail.com>
2017-06-09 12:36:31 -0400

    fix(mediaplayer): add Kendo mediaplayer wrapper

---
+++ sample/src/test3/test3.js
@@ -0,0 +1,26 @@
+export class Test3 {
+
+    onPlay() {
+        console.log("onPlay");
+    }
+
+    onPause() {
+        console.log("onPause");
+    }
+
+    onEnd() {
+        console.log("onEnd");
+    }
+
+    onReady() {
+        console.log("onReady");
+    }
+
+    onTimeChange() {
+        console.log("onTimeChange");
+    }
+    onVolumeChange() {
+        console.log("onVolumeChange");
+    }
+
+}
