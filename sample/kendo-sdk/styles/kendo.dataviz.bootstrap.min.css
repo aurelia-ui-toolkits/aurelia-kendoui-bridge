c41cc5a669ab236814aab8dcfa5a8eb1348bcd97
JeroenVinke <jeroenvinke17@gmail.com>
2017-06-29 17:06:55 +0200

    chore(all): update kendo

--- sample/kendo-sdk/styles/kendo.dataviz.bootstrap.min.css
+++ sample/kendo-sdk/styles/kendo.dataviz.bootstrap.min.css
@@ -1,5 +1,5 @@
 /** 
- * Kendo UI v2017.1.223 (http://www.telerik.com/kendo-ui)                                                                                                                                               
+ * Kendo UI v2017.2.621 (http://www.telerik.com/kendo-ui)                                                                                                                                               
  * Copyright 2017 Telerik AD. All rights reserved.                                                                                                                                                      
  *                                                                                                                                                                                                      
  * Kendo UI commercial licenses may be obtained at                                                                                                                                                      
--- sample/src/test/test.js
+++ sample/src/test/test.js
@@ -1,16 +1,2 @@
 export class Test {
-  data = new kendo.data.DataSource({
-        data : [
-      { text: 'Cotton', value: '1', desc:'Cotton /Looong desc' },
-      { text: 'Polyester', value: '2', desc:'Poly /Looong desc' },
-      { text: 'Cotton/Polyester', value: '3', desc:'Both /Looong desc' },
-      { text: 'Rib Knit', value: '4', desc:'Rib /Looong desc' }
-      ]
-    });
-
-    fabric=3;
-
-  onReady(w){
-    // w.setOptions({autoWidth:true});
-  }
 }
